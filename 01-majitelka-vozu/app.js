3292cb8dbdd10a733aab2da7813e330c7ddfb0b0
Adéla Krásová <>
2022-04-05 16:08:25 +0200

    Majitelka vozu - vypis z pole objektu do UI

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -15,6 +15,7 @@ auto.majitel.prijmeni = 'Czechita';
 
 // Přepiš značku na Škoda
 auto['znacka']='Škoda';
+auto.barva = "červená";
 
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
 let majitelka = auto.majitel.jmeno + ' '+ auto.majitel.prijmeni;
@@ -22,3 +23,10 @@ let znacka = auto.znacka;
 let SPZ = auto.spz;
 
 console.log(auto);
+console.log(majitelka);
+console.log(znacka);
+console.log(SPZ);
+
+document.getElementById("majitelka").innerHTML = majitelka+", ";
+document.getElementById("znacka").innerHTML = znacka;
+document.getElementById("spz").innerHTML = SPZ;
